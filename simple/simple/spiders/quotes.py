ceb841b377ee1a6a78d3706deb165fff650a38d6
dannv6 <dannv6@fpt.com.vn>
2018-10-24 16:04:54 +0700

    Scraping Details Pages

--- simple/simple/spiders/quotes.py
+++ simple/simple/spiders/quotes.py
@@ -5,7 +5,7 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['toscrape.com']
-    start_urls = ['http://toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
         self.log("I just visited " + response.url)
@@ -22,3 +22,16 @@ class QuotesSpider(scrapy.Spider):
         if next_page_url:
             next_page_url = response.urljoin(next_page_url)
             yield scrapy.Request(url=next_page_url, callback=self.parse)
+        
+
+        # details pages from listings
+        urls = response.css('div.quote > span > a::attr(href)').extract()
+        for url in urls:
+            url = response.urljoin(url)
+            yield scrapy.Request(url=url, callback=self.parse_details)
+    
+    def parse_details(self, response):
+        yield {
+            'name': response.css('h3.author-title::text').extract_first(),
+            'birthday': response.css('span.author-born-date::text').extract_first(),
+        }
