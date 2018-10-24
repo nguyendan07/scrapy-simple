e13562cd29da3d6a1cb6ecc8a3ee33a45c42233c
dannv6 <dannv6@fpt.com.vn>
2018-10-24 16:04:08 +0700

    Following Pagination Links

--- simple/simple/spiders/quotes.py
+++ simple/simple/spiders/quotes.py
@@ -8,4 +8,17 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://toscrape.com/']
 
     def parse(self, response):
-        pass
+        self.log("I just visited " + response.url)
+        for quote in response.css('div.quote'):
+            item = {
+                'author_name': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract(),
+                }
+            yield item
+        
+        # follow pagination link
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
